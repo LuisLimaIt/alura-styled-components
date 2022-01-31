4ce20e368fbaa514a7a9c6b5f4b45b79a32a8eb1
luislimait <pls.luis@gmail.com>
2022-01-31 17:26:29 -0300

    Itens

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Box, Botao } from '../UI';
 import { extratoLista } from '../../info';
+import Items from '../Items';
 
 const Extrato = () => {
   return <Box> 
     {extratoLista.updates.map(({ id, type, from, value, date }) => {
       return(
-        <div key={id}>
-          <div>{type}</div>
-          <div>{from}</div>
-          <Botao>Ver Mais</Botao>
-        </div>
-      )
-    }
-  )} </Box>
+        <Items key={id} type={type} from={from} value={value} date={date} />
+      );
+    })} 
+  <Botao>Ver Mais</Botao>
+  </Box>
 };
 
 export default Extrato;
